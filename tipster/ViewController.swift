b169c09cfe9577f28bdd51630724b7ad1d1404d2
Anna Propas <apropas@gmail.com>
2017-07-06 13:50:08 -0700

    Initial Commit

---
+++ tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Anna Propas on 7/6/17.
+//  Copyright © 2017 Anna Propas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
